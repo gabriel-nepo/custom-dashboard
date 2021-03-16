d8926830169d2d7f01cf8e61f34286e589a94c64
Gabriel Campos <36010861+gabriel-nepo@users.noreply.github.com>
2021-03-16 14:10:55 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "./isAuthenticated";
 
 const api = axios.create({
-  // baseURL: "https://sensorial-refri-backend.herokuapp.com/"
-  baseURL: "http://localhost:3000/"
+  baseURL: "https://sensorial-refri-backend.herokuapp.com/"
+  //baseURL: "http://localhost:3000/"
 });
 
 api.interceptors.request.use(async config => {
